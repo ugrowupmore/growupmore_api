b6d74df716fc822c00dd62cbcfc91d807ed08e3d
Girish Chaudhary <girishinindia@gmail.com>
2024-12-25 16:09:32 +0530

    delete all

--- README.md
+++
@@ -1,1 +0,0 @@
-growupmore_api
--- authuser/authentication/__init__.py
+++
@@ -1,7 +0,0 @@
-# authuser/authentication/__init__.py
-
-from .unified_jwt_authentication import UnifiedJWTAuthentication
-
-__all__ = [
-    'UnifiedJWTAuthentication',
-]
